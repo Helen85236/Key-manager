23ecd28f45d8f2d2ba0d73a2b027acf383505c09
Helen85236 <79532870251@yandex.ru>
2023-11-12 19:51:35 +0300

    Add users and safe secret

---
+++ safe_secret/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SafeSecretConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'safe_secret'
---
+++ safe_secret/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from safe_secret.apps import SafeSecretConfig
+
+
+app_name = SafeSecretConfig.name
+
+
+urlpatterns = []
