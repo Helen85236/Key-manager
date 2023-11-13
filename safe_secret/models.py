8ef1419c841538b6fac52b54c7e31f44c6a8c346
Helen85236 <79532870251@yandex.ru>
2023-11-13 20:59:43 +0300

    Model safe_secret

--- safe_secret/models.py
+++ safe_secret/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Secret(models.Model):
+    hash = models.CharField(max_length=64, verbose_name='Хэш')
+    ciphertext = models.TextField(verbose_name='Зашифрованный текст')
+    link = models.CharField(max_length=250, verbose_name='Ссылка')
+    lifetime = models.PositiveSmallIntegerField(verbose_name='Время жизни')
+    code_frase = models.BooleanField(default=True, **NULLABLE)
+
+    def __str__(self):
+        return f'Секрет {self.pk}'
+
+    class Meta:
+        verbose_name = 'Секрет'
+        verbose_name_plural = 'Секреты'
